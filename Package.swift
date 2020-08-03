46a1dcc907f81e205b29baffa600891d4da1cead
Alexey Ivanov <alexey.i@mercaux.com>
2020-08-03 22:22:48 +0300

    minor changs

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ClibICA",
-            targets: ["ClibICA"]),
+            targets: ["ClibICA"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,6 +26,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+//        .systemLibrary(name: "ClibICA")
         .target(
             name: "ClibICA",
             dependencies: [])
